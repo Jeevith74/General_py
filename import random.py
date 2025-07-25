55f9d951bc17072c7f82be12b3c20eed9930b1ba
Jeevith gowda <jeevithgowda74@gmail.com>
2025-07-25 09:50:26 +0530

    modified user experience

--- import random.py
+++ import random.py
@@ -1,13 +1,10 @@
 import random
 
-# TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 from hangman_words import word_list
 from hangman_art import stages,logo
 print(logo)
 
 lives = 6
-
-# TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 
 chosen_word = random.choice(word_list)
 
@@ -21,12 +18,9 @@ game_over = False
 correct_letters = []
 
 while not game_over:
-
-    # TODO-6: - Update the code below to tell the user how many lives they have left.
     print(f"****************************You have : {lives}/6 LIVES LEFT****************************")
     guess = input("Guess a letter: ").lower()
 
-    # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in correct_letters:
         print(f"you have already guessed the letter : {guess}")
     display = ""
@@ -42,9 +36,6 @@ while not game_over:
 
     print("Word to guess: " + display)
 
-    # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
-    #  e.g. You guessed d, that's not in the word. You lose a life.
-
     if guess not in chosen_word:
         print(f"the letter : {guess}, is not in the CHOSEN WORD , You lose a life")
         lives -= 1
@@ -53,12 +44,10 @@ while not game_over:
         if lives == 0:
             game_over = True
 
-            # TODO 7: - Update the print statement below to give the user the correct word they were trying to guess.
             print(f"***********************It was : {chosen_word}, YOU LOSE**********************")
 
     if "_" not in display:
         game_over = True
         print("****************************YOU WIN****************************")
 
-    # TODO-2: - Update the code below to use the stages List from the file hangman_art.py
     print(stages[lives])
